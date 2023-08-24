b59434bab6c189b40981205f8b89e401c1609396
MudassirKhan22 <khanmudassir2000@gmail.com>
2023-08-24 16:40:40 +0530

    Update Jenkins

--- server-config.sh
+++ server-config.sh
@@ -6,6 +6,7 @@ sudo yum install maven -y
 if [ -d "addressbook" ]
 then
     echo "Repo is already cloned and exists"
+    cd /home/ec2-user/addressbook
     git pull origin JenkinsCopy
 else
     git clone https://github.com/MudassirKhan22/addressbook.git
